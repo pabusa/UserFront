9d65b684536102146faf5f9c243b895ab93fe21d
pabusa <pabusa@gmail.com>
2017-09-18 02:16:20 +0200

    Added userFront page

--- src/main/java/com/userfront/controller/AccountController.java
+++ src/main/java/com/userfront/controller/AccountController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+	
 	@RequestMapping("/primaryAccount")
 	public String primaryAccount (){
 		return "primaryAccount";
